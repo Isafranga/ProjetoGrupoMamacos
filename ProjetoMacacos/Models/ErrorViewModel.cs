68870a7d716cf56ed88f8b34c18e15720ea5ac95
Isafranga <isadora.a.oliveira6@aluno.senai.br>
2025-10-21 14:46:49 -0300

    Projeto

---
+++ ProjetoMacacos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetoMacacos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
